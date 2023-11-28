3b9463868f3cb28344b6fc6dc645857c95355f95
Oleg Kiryushin <fewgwer3@yandex.ru>
2023-11-28 13:25:42 +0300

    namespace-refactoring2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './app.jsx';
+import App from './App.jsx';
 
 import store from './store/store.js';
 
