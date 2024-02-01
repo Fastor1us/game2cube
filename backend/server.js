8d5087aa4489264574fe8e00ab0b5e047515ea83
Oleg Kiryushin <fewgwer3@yandex.ru>
2024-02-01 20:54:13 +0300

    v0.7.0

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,15 @@ const EXTRA_EXTERNAL_URL = process.env.EXTRA_EXTERNAL_ACCESS_ORIGIN || null;
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const cors = {
+  origin: [CLIENT_URL, EXTRA_EXTERNAL_URL],
+  default: CLIENT_URL
+}
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', CLIENT_URL);
-  if (EXTRA_EXTERNAL_URL) {
-    res.setHeader('Access-Control-Allow-Origin', EXTRA_EXTERNAL_URL);
-  }
+  res.header("Access-Control-Allow-Origin",
+    cors.origin.includes(req.headers.origin) ? req.headers.origin : cors.default
+  );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
